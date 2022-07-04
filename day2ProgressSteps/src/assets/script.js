2ed00e3f8cbbee7013550719a82833ebc88e0773
ArnoWeichuXu <arno.xu.weichu@gmail.com>
2022-07-04 18:08:54 +1000

    day2 added

---
+++ day2ProgressSteps/src/assets/script.js
@@ -0,0 +1,12 @@
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+
+prev.addEventListener("click", () => {
+  /** TODO: */
+});
+
+next.addEventListener("click", () => {
+  /** TODO: */
+});
+prev.classList.remove("active");
+next.classList.add("active");
