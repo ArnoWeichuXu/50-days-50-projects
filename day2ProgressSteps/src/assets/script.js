37d0eb255262dfcf5e22f57d1728a010722b507b
ArnoWeichuXu <arno.xu.weichu@gmail.com>
2022-07-05 17:28:05 +1000

    day2 update

--- day2ProgressSteps/src/assets/script.js
+++ day2ProgressSteps/src/assets/script.js
@@ -1,12 +1,52 @@
 const prev = document.querySelector(".prev");
 const next = document.querySelector(".next");
+const line = document.querySelectorAll(".line");
+const circles = document.querySelectorAll(".circle");
 
-prev.addEventListener("click", () => {
-  /** TODO: */
-});
+let currentActive = 0;
 
 next.addEventListener("click", () => {
-  /** TODO: */
+  currentActive++;
+  if (currentActive > line.length) {
+    currentActive = line.length;
+  }
+  update();
 });
-prev.classList.remove("active");
-next.classList.add("active");
+
+prev.addEventListener("click", () => {
+  currentActive--;
+  if (currentActive < 1) {
+    currentActive = 0;
+  }
+  update();
+});
+
+function update() {
+  line.forEach((lineNode, index) => {
+    if (index < currentActive) {
+      lineNode.classList.add("active");
+    } else {
+      lineNode.classList.remove("active");
+    }
+  });
+  circles.forEach((circleNode, index) => {
+    if (index < currentActive) {
+      circleNode.classList.add("active");
+    } else {
+      circleNode.classList.remove("active");
+    }
+  });
+  if (currentActive === 0) {
+    prev.disabled = true;
+    prev.classList.remove("active");
+  } else if (currentActive === line.length) {
+    next.disabled = true;
+    next.classList.remove("active");
+  } else {
+    prev.disabled = false;
+    prev.classList.add("active");
+    next.disabled = false;
+    next.classList.add("active");
+  }
+  console.log(currentActive);
+}
